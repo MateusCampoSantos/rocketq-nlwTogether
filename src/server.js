968eec028212571baf37ed7e8fe27783c39eec02
Mateus Campos <mateuscamposantos@gmail.com>
2021-06-24 17:48:44 -0300

    Configurando EJS

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const route = require("./routes");
+const path = require("path");
 
 const port = 3000;
 const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(route);
 
 app.listen(port, () => {
   console.log("RODANDO");

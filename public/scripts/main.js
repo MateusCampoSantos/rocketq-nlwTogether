fc00a88fabeb610b604ebb8febde84d6aaa00afa
Mateus Campos <mateuscamposantos@gmail.com>
2021-06-24 18:23:10 -0300

    Update main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,4 +2,14 @@ import { Modal } from "./modal";
 
 const modal = Modal();
 
+// Pegando todos os botões com class check
+const checkButtons = document.querySelectorAll(".actions a.check");
+
+checkButtons.forEach((button) => {
+  button.eventlistener("click", (event) => {
+    modal.open();
+  });
+});
+
+//Abrir modal
 modal.open();

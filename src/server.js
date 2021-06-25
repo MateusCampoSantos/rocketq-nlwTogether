01c362fd63beb0386b95453feb6f5aae8232ddff
Mateus Campos <mateuscamposantos@gmail.com>
2021-06-25 11:05:18 -0300

    Controller questionController

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 app.use(route);
 
 app.listen(port, () => {

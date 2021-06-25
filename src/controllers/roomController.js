c456e5382e984fbbe83da2568564457cd845bb27
Mateus Campos <mateuscamposantos@gmail.com>
2021-06-25 12:17:30 -0300

    Update roomController.js

--- src/controllers/roomController.js
+++ src/controllers/roomController.js
@@ -3,7 +3,9 @@ module.exports = {
     let roomId;
 
     for (var i = 0; i < 6; i++) {
-      roomId += Math.floor(Math.random() * 10).toString();
+      i == 0
+        ? (roomId += Math.floor(Math.random() * 10).toString())
+        : (roomId += Math.floor(Math.random() * 10).toString());
     }
 
     res.redirect(`/room/${roomId}`);

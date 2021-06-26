5e72df5f7546f73a149ffc049c34f6fc334407ad
Mateus Campos <mateuscamposantos@gmail.com>
2021-06-26 20:33:07 -0300

    Termino do projeto

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,7 @@ route.get("/create-pass", (req, res) => {
 });
 
 route.get("/room/:room", roomController.open);
+route.post("/enterroom", roomController.enter);
 route.post("/create-room", roomController.create);
 
 route.post("/question/:room/:question/:action", questionController.index);

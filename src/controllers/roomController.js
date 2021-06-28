a16fd85ca044b239ea9a89069fcaba509ca67395
Mateus Campos <mateuscamposantos@gmail.com>
2021-06-28 11:31:20 -0300

    Corrigindo o isNoQuestions

--- src/controllers/roomController.js
+++ src/controllers/roomController.js
@@ -41,16 +41,16 @@ module.exports = {
     const db = await Database();
     const roomId = req.params.room;
     const questions = await db.all(
-      `SELECT * FROM questions WHERE room = ${roomId} AND read = 0`
+      `SELECT * FROM questions WHERE room = ${roomId} and read = 0`
     );
     const questionsRead = await db.all(
-      `SELECT * FROM questions WHERE room = ${roomId} AND read = 1`
+      `SELECT * FROM questions WHERE room = ${roomId} and read = 1`
     );
     let isNoQuestions;
 
     if (questions.length == 0) {
       if (questionsRead.length == 0) {
-        isNoQuestions = false;
+        isNoQuestions = true;
       }
     }
 
